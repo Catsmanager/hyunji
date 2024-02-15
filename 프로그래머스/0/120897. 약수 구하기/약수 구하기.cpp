c49294078235d88b9d51089ed16a7c55a2c0bc38
Hyunji <132527976+Catsmanager@users.noreply.github.com>
2024-02-15 10:25:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) 
+{
+    vector<int> answer;
+
+    for(int i=1;i<=10000;i++)
+    {
+        if(n%i==0)
+            answer.push_back(i);
+    }
+    return answer;
+}
