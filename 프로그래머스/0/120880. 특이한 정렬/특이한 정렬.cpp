577f1a4de06bcac13e9d78d3bbc1995c09cd3ceb
Hyunji <hyunhyun0096@naver.com>
2024-03-28 18:29:00 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include<cmath>
+
+using namespace std;
+
+vector<int> solution(vector<int> numlist, int n) {
+    int temp;
+    
+    for(int i=0;i<numlist.size()-1;i++)
+    {
+        for(int j=i;j<numlist.size();j++)
+        {
+           
+             if(abs(n-numlist[i])>abs(n-numlist[j]))
+            {
+                temp=numlist[i];
+                numlist[i]=numlist[j];
+                numlist[j]=temp;
+            }
+             if(abs(n-numlist[i])==abs(n-numlist[j])&&numlist[i]<numlist[j])
+             {
+                 temp=numlist[j];
+                 numlist[j]=numlist[i];
+                 numlist[i]=temp;
+             }
+        }
+        
+    }
+    return numlist;
+}
